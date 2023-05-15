befd787e4c2483ba54e30cce8bb637919c11dbec
Kushian01100111 <comaspedro6@gmail.com>
2023-05-14 21:11:04 -0300

    14/05

--- frontend/src/components/homepage componets/testimonios/Testimonios.jsx
+++ frontend/src/components/homepage componets/testimonios/Testimonios.jsx
@@ -1,9 +1,12 @@
+import "./Testimonios.css"
 import Testimonials from '../../testimonials/Testimonials'
 
 const Testimonios = () => {
   return (
-    <div>
-        <Testimonials name={"emilia gazotti"} company={"Vichka Oficial"} exp={"Conocí a la agencia Frenzy desde sus inicios. Es una agencia con la que podes contar para crear una estrategia 360 de comunicación para tu empresa.Entendiendo 100% el mundo digital proponen también ideas para conectar con el mundo físico. Lo logran teniendo un equipo creativo, responsable, innovador y alegre. Trabajar con las chicas de Frenzy siempre es un verdadero placer."} img={"https://images.unsplash.com/photo-1552374196-c4e7ffc6e126?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"}/>
+    <div className='testShowCase'>
+        <Testimonials name={"emilia gazotti"} company={"Vichka Oficial"} position={"Co-Fundadora"} exp={"conocí a la agencia frenzy desde sus inicios. es una agencia con la que podes contar para crear una estrategia 360 de comunicación para tu empresa"} extraExp={". entendiendo 100% el mundo digital proponen también ideas para conectar con el mundo físico. lo logran teniendo un equipo creativo, responsable, innovador y alegre. trabajar con las chicas de frenzy siempre es un verdadero placer."} img={"https://res.cloudinary.com/dpsobnvtv/image/upload/v1684079156/Frenzy/Captura_htfmzn.png"}/>
+        <Testimonials name={"agustina petralia"} company={"Desiderata, Grupo Mazalosa"} position={"Analista de marketing"} exp={`trabajar con el equipo de trenzy es casi como tener un equipo indoors. si tuviera que reducirlo solo en una frase diria que : "Tienen la camiseta puesta" y eso para mi vale oro`} extraExp={". La dedicacion, el compromiso, la respuesta inmediata. Aceptar las devoluciones y construir cada dia una dinamica mas consolidada y poderosa que se traduce en una mejora constante de resultdos. tengo la suerte de haber visto el crecimiento exponecial que tuvo el ultimo año, y solo puedo decir que confio cada dia mas en las manos y en la cabeza de una agencia que esta 100% comprometida con mi marca."} img={"https://res.cloudinary.com/dpsobnvtv/image/upload/v1684107306/Rectangle_3857_awwf2t.png"}/>
+        <Testimonials name={"emilia gazotti"} company={"Vichka Oficial"} position={"Co-Fundadora"} exp={"conocí a la agencia frenzy desde sus inicios. es una agencia con la que podes contar para crear una estrategia 360 de comunicación para tu empresa"} extraExp={". entendiendo 100% el mundo digital proponen también ideas para conectar con el mundo físico. lo logran teniendo un equipo creativo, responsable, innovador y alegre. trabajar con las chicas de frenzy siempre es un verdadero placer."} img={"https://res.cloudinary.com/dpsobnvtv/image/upload/v1684079156/Frenzy/Captura_htfmzn.png"}/>
     </div>
   )
 }
--- frontend/src/components/testimonials/Testimonials.jsx
+++ frontend/src/components/testimonials/Testimonials.jsx
@@ -1,8 +1,14 @@
 import "./../testimonials/Testimonial.css"
+import { useState } from "react"
 import {motion as m} from "framer-motion"
 
 
-const Testimonials = ({name,company,exp,img}) => {
+const Testimonials = ({name,company,position,exp,extraExp,img}) => {
+  const [readMore, setReadMore] = useState(false)
+  const onClick = ()=>{
+    setReadMore(!readMore)
+  }
+
   return (
     <div>
       <div className="cardTest">
@@ -10,13 +16,14 @@ const Testimonials = ({name,company,exp,img}) => {
             <div>
                 <h2>{name}</h2>
                 <span>{company}</span>
-            </div>
+                <span>{position}</span>
+            </div>  
             <div>
-               {exp}
+               <p>{exp}<m.span className={`longtext ${readMore ? "readMore": "clickReadMore"}`} onClick={onClick}>{readMore ? extraExp: "..."}</m.span></p>
             </div>
         </div>
         <m.div
-          animate={{y:5}} 
+          animate={{y:50}} 
           className="preContainerTest">
             <div className="containerTest">
                 <div style={{backgroundImage: `url(${img})`}} className="imgTest"></div>
